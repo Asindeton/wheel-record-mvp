54b8980e5a2445e7c4c19b2769c1249aca1f6474
legankov95 <legankov95@gmail.com>
2023-07-24 20:20:39 +0400

    fix: fix customer auth system

--- src/pages/Employe/EmployeeAuth/authSlice.ts
+++ src/pages/Employe/EmployeeAuth/authSlice.ts
@@ -14,23 +14,20 @@ const slice = createSlice({
   initialState: initialState,
   reducers: {
     setCredentials: (state, { payload }: PayloadAction<ILoginResponse>) => {
-      localStorage.setItem('token', payload.access_token);
-      localStorage.setItem('token_type', payload.token_type);
       state.token_type = payload.token_type;
       state.expires_in = payload.expires_in;
       state.access_token = payload.access_token;
     },
     checkToken: (state) => {
-      const token = localStorage.getItem('token');
-      const token_type = localStorage.getItem('token_type');
+      const token = import.meta.env.VITE_AUTH_TOKEN;
+      const token_type = import.meta.env.VITE_AUTH_TYPE;
+      console.log('token', token);
       if (token && token_type) {
         state.access_token = token;
         state.token_type = token_type;
       }
     },
     logout: () => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('token_type');
       return initialState;
     },
   },
