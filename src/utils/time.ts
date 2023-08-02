b2991f2a9b4f5f3b09e996a1d8d0ed95185cd443
legankov95 <legankov95@gmail.com>
2023-08-02 10:08:35 +0400

    fix: change time function to short

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,9 +2,13 @@ import { ITimesInStatus } from '../api/queue/QueueApi.ts';
 
 export const getTime = ({ d, h, i, s }: ITimesInStatus) => {
   let result = '';
-  if (d) result += `${d} дней `;
-  if (h) result += h === 1 ? `${h} час ` : h > 4 ? `${h} часов ` : `${h} часа `;
-  if (i) result += `${i} минут `;
-  if (s) result += `${s} секунд `;
+  // if (d) result += `${d} дней `;
+  // if (h) result += h === 1 ? `${h} час ` : h > 4 ? `${h} часов ` : `${h} часа `;
+  // if (i) result += `${i} минут `;
+  // if (s) result += `${s} секунд `;
+  if (d) result += `${d} д `;
+  if (h) result += `${h} ч `;
+  if (i) result += `${i} м `;
+  if (s) result += `${s} с `;
   return result;
 };
